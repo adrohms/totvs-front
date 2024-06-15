903adcab8d50609a2d5314ac18f3eff084be73d8
adrohms <adrom7.0@gmail.com>
2024-06-15 12:49:47 -0300

    Improving style and making my marketing

--- src/app/modules/account/account.routing.ts
+++ src/app/modules/account/account.routing.ts
@@ -2,6 +2,7 @@ import { Route } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserManagementComponent } from './user-management/user-list/user-management.component';
+import { routeAccessGuard } from 'src/app/core/auth/user-route-access.guard';
 
 export const accountRouting: Route[] = [
   {
@@ -21,5 +22,6 @@ export const accountRouting: Route[] = [
   {
     path: 'gerenciar',
     loadChildren: () => import('./user-management/user-management.module').then(m => m.UserManagementModule),
+    canActivate: [routeAccessGuard]
   }
 ];
